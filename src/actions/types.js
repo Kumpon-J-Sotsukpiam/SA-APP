10a4f922370c096d60ee290ec84a78ddd65953ab
Kumpon J Sotsukpiam <tetravaal.system@gmail.com>
2019-10-22 18:46:03 +0700

    update tpye of evens

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,25 @@ export const GET_ERRORS = 'GET_ERRORS'
 // Token Even Type
 export const SET_CURRENT_TOKEN = 'SET_CURRENT_TOKEN'
 export const UNSET_CURRENT_TOKEN = 'UNSET_CURRENT_TOKEN'
+
+// Student Even Type
+export const ADD_STUDENT = 'ADD_STUDENT'
+export const DELETE_STUDENT = 'DELETE_STUDENT'
+export const SET_STUDENT = 'SET_STUDENT'
+export const GET_STUDENT = 'GET_STUDENT'
+
+// Course Even Type
+export const ADD_COURSE = 'ADD_COURSE'
+export const DELETE_COURSE = 'DELETE_COURSE'
+export const SET_COURSE = 'SET_COURSE'
+export const GET_COURSE = 'GET_COURSE'
+
+// Class Even Type
+export const ADD_CLASS = 'ADD_CLASS'
+export const DELETE_CLASS = 'DELETE_CLASS'
+export const SET_CLASS= 'SET_CLASS'
+export const GET_CLASS = 'GET_CLASS'
+
+// Check-Student Even Type
+export const CHECK_STUDENT = 'CHECK_STUDENT'
+export const UNCHECK_STUDENT = 'UNCHECK_STUDENT'
