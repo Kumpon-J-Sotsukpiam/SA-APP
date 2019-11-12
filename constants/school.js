7726988193ee4334f4881a092b7310432b876df1
moochamp <chanathip-n@hotmail.com>
2019-11-12 22:46:17 +0700

    update constants school

--- constants/school.js
+++ constants/school.js
@@ -1,92 +1,88 @@
 const schoolList = 
 [{
     "faculty":{
-        "id":"1",
+        "id":"01",
         "name":"School of Business",
-        "major":["Marketing",
-                "International Business Management",
-                "Finance",
-                "Management",
-                "Business Computer",
-                "Logistics Management",
-                "Game and eSports",
-                "Innovation Driven Entrepreneurship"]
+        "major":[{"id":"01","name":"Marketing"},
+                 {"id":"02","name":"International Business Management"},
+                 {"id":"03","name":"Finance"},
+                 {"id":"04","name":"Human Resource Management"},
+                 {"id":"05","name":"Management"},
+                 {"id":"06","name":"Industrial Management"},
+                 {"id":"07","name":"Hotel Management"},
+                 {"id":"08","name":"Tourism Management)"},
+                 {"id":"09","name":"Business Computer"},
+                 {"id":"10","name":"Logistics Management"},
+                 {"id":"11","name":"Entrepreneurship"},
+                 {"id":"11","name":"Bussiness Administration"},]
         }
 
 },{
     "faculty":{
-        "id":"2",
+        "id":"02",
         "name":"School of School of Accountancy",
-        "major":["Accountancy",
-                "Accountancy (International Program)",]
+        "major":[{"id":"00","name":"Accountancy"},]
         }
 },{
     "faculty":{
-        "id":"3",
+        "id":"03",
         "name":"School of Economics",
-        "major":["Economics"]
+        "major":[{"id":"00","name":"Economics"}]
         }
 },{
     "faculty":{
-        "id":"4",
+        "id":"04",
         "name":"School of Humanities and Applied Arts",
-        "major":["Business English (Bilingual Program)",
-                 "English for Business Communication",
-                "Japanese",
-                "English and Translation",
-                "Thai Language for Communication",
-                "Chinese",
-                "Performing Arts",
-                "Korean",
-                "Interdisciplinary Studies"]
+        "major":[{"id":"00","name":"English for Business Communication"},
+                 {"id":"01","name":"English for Business Communication"},
+                 {"id":"02","name":"English and Translation"},
+                 {"id":"03","name":"Thai Language for Communication"},
+                 {"id":"04","name":"Japanese"},
+                 {"id":"05","name":"Information Studies"},
+                 {"id":"06","name":"Chinese"},
+                 {"id":"07","name":"Performing Arts"},
+                 {"id":"08","name":"Tourism"},
+                 {"id":"09","name":"Korean"},
+                 {"id":"10","name":"Airline Business Management"},
+                 {"id":"11","name":"PConvention Exhibition and Event Management"},
+                 {"id":"12","name":"Chinese for Career"}]
         }
 },{
     "faculty":{
-        "id":"5",
+        "id":"05",
         "name":"School of Science and Technology",
-        "major":["Computer Science",
-                "Food Business Management",
-                "Food Science and Technology",
-                "Financial Engineering",
-                "Computer Animation",
-                "Information and Communication Technology",
-                "Digital Technology",
-                "Food Innovation",
-                "Interdisciplinary Studies"]
+        "major":[{"id":"01","name":"Computer Science"},
+                 {"id":"02","name":"Food Business Management"},
+                 {"id":"03","name":"Food Science and Technology"},
+                 {"id":"04","name":"Financial Engineering"},
+                 {"id":"05","name":"Computer Animation"},
+                 {"id":"06","name":"Information and Communication Technology"},
+                 {"id":"07","name":"Digital Technology"},
+                 {"id":"08","name":"Food Innovation"},
+                 {"id":"09","name":"Interdisciplinary Studies"}]
         }
 
 },,{
     "faculty":{
-        "id":"6",
+        "id":"06",
         "name":"School of Communication of Arts",
         "major":["Communication Arts"]
         }
 },{
     "faculty":{
-        "id":"7",
+        "id":"07",
         "name":"School of Engineering'",
-        "major":["Electrical and Energy Engineering",
-                 "Logistics Engineering",
-                "Computer Engineering and Artificial Intelligence",
-                "Rail Business Innovation Engineering",
-                "Automotion Innovation Engineering",]
+        "major":[{"id":"01","name":"Electrical and Energy Engineering"},
+                 {"id":"02","name":"Computer Engineering and Artificial Intelligence"},
+                 {"id":"03","name":"Logistics Engineering"},
+                 {"id":"04","name":"Rail Business Innovation Engineering"},
+                 {"id":"05","name":"Automotion Innovation Engineering"},]
         }
 },{
     "faculty":{
-        "id":"8",
+        "id":"08",
         "name":"School of Law",
-        "major":["Law"]
-        }
-},{
-    "faculty":{
-        "id":"9",
-        "name":"School of School of Tourism and Services",
-        "major":["Tourism'",
-                 "Hotel Management",
-                "Tourism Management",
-                "Airline Business Management",
-                "Event Management",
-                "Tourism Management (International Program)"]
+        "major":[{"id":"00","name":"Law"}]
         }
 }]
 
