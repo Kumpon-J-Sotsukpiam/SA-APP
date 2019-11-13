845667f00ccf56799f4034d9f619efdbf27a56a9
Kumpon J Sotsukpiam <tetravaal.system@gmail.com>
2019-11-13 13:36:36 +0700

    search student id at student list Screen

--- screens/StudentListScreen.js
+++ screens/StudentListScreen.js
@@ -13,7 +13,7 @@ import { createFilter } from 'react-native-search-filter';
 import { connect } from 'react-redux'
 import { pull_student_in_class } from '../src/actions/class'
 import { train_model, check_status_model } from '../src/actions/model'
-const KEYS_TO_FILTERS = ['name', 'stuId'];
+const KEYS_TO_FILTERS = ['stuId', 'name','faculty','major'];
 class ButtonTrain extends React.Component {
   constructor(props) {
     super(props)
@@ -75,7 +75,8 @@ class StudentListScreen extends React.Component {
     console.log('this cancel train');
   }
   render() {
-    //const filteredStudent = this.state.dataStudent.filter(createFilter(this.state.search, KEYS_TO_FILTERS))
+    const propsStudent = this.props.student.filter(i => this.state.class.studentList.indexOf(i._id >= 0))
+    const filteredStudent = propsStudent.filter(createFilter(this.state.search,KEYS_TO_FILTERS))
     return (
       <View style={styles.container}>
         <Header
@@ -124,8 +125,8 @@ class StudentListScreen extends React.Component {
         </View>
 
         <ScrollView>
-          {this.state.class.studentList.map(dataStudent => {
-            const { _id, name, stuId } = this.props.student.filter(i => i._id == dataStudent)[0]
+          {filteredStudent.map(dataStudent => {
+            const { _id, name, stuId } = dataStudent
             return (
               <View key={_id} style={{ backgroundColor: '#f3f3f3', margin: 3, borderRadius: 10 }}>
                 <Swipeout left={[{
