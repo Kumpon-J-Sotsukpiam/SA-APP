c9b660b262631b2b7ccc071cf7bc003b3d39487d
server express <touchzaa007@gmail.com>
2019-11-23 12:55:02 +0700

    update login fb

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import * as FaceDetector from 'expo-face-detector'
  //kumpon df if = 192.168.43.216 and gateway 10.0.2.2
  //champ 192.168.1.38
-export const ip_server = '192.168.1.38' // my ip address
+export const ip_server = '172.20.10.7' // my ip address
 export const port = 3001
 export const deep_port = 5000
 export const server_url = `http://${ip_server}:${port}`
